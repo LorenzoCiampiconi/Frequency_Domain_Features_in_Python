fa43e62fc8931072b18b5ac27670465f458d7995
LorenzoCiampiconi <lorenzo.ciampiconi@mail.polimi.it>
2018-04-18 17:02:41 +0800

    Com

--- FileHandler.py
+++ FileHandler.py
@@ -1,12 +1,13 @@
 import scipy.io.wavfile
 import numpy
+import librosa as lib
 from WavInfo import WavInfo
 
 #Define path and useful variables such as the separator used in the file to be queried
-read_path = "C:/Users/Lorenzo/Documents/Polimi/Singapore/Sound and Music Computing/Assignment/#1/music_speech/"
-output_path = "C:/Users/Lorenzo/Documents/Polimi/Singapore/Sound and Music Computing/Assignment/#1"
-query_list_file_name = "music_speech.mf"
-output_file_name = "#1outputB.arff"
+read_path = "C:/Users/Lorenzo/Documents/Polimi/Singapore/Sound and Music Computing/Assignment/Final Project/"
+output_path = "C:/Users/Lorenzo/Documents/Polimi/Singapore/Sound and Music Computing/Assignment/Final Project/Feauters Extracted"
+query_list_file_name = "genre-classification.mf"
+output_file_name = "Frequency-Domain.arff"
 sep = "\t"
 range = 1024
 hop = 512
@@ -14,7 +15,7 @@ hop = 512
 header_lines = ["@RELATION music_speech","@ATTRIBUTE SC_MEAN NUMERIC","@ATTRIBUTE SRO_MEAN NUMERIC",
                 "@ATTRIBUTE SFM_MEAN NUMERIC","@ATTRIBUTE PARFFT_MEAN NUMERIC","@ATTRIBUTE FLUX_MEAN NUMERIC",
                 "@ATTRIBUTE SC_STD NUMERIC","@ATTRIBUTE SRO_STD NUMERIC","@ATTRIBUTE SFM_STD NUMERIC",
-                "@ATTRIBUTE PARFFT_STD NUMERIC","@ATTRIBUTE FLUX_STD NUMERIC","@ATTRIBUTE class {music,speech}"]
+                "@ATTRIBUTE PARFFT_STD NUMERIC","@ATTRIBUTE FLUX_STD NUMERIC","@ATTRIBUTE class {blues, classical, country, disco, hiphop, jazz, metal, pop, reggae, rock}"]
 
 data = "@DATA"
 
@@ -26,10 +27,10 @@ def read_files(wav_list):
 
     #open the file
     with open(read_path + query_list_file_name, "r") as query_list_file:
-
+        j = 0
         #read each line
         for line in query_list_file:
-
+            print(j)
             #instantiate new WavInfo object
             wav_info = WavInfo()
 
@@ -40,8 +41,9 @@ def read_files(wav_list):
             wav_info.label = line.partition(sep)[2].strip('\n')
 
             #Reading the samples from the wav file
-            samples = scipy.io.wavfile.read(read_path + wav_info.file_name)[1]
+            samples = lib.load(read_path + wav_info.file_name)[0]
 
+            j += 1
 
             reading = True
             i = 0
@@ -76,7 +78,11 @@ def write_output(wav_list):
 
         output_file.write(data + "\n")
         #read each object
+
+        i = 0
         for wav_info in wav_list:
+
+            print(i)
 
             #composing the string to be written, reformatting the float values, first the Means, stripping the "[]"
             line = str(['%.6f' % elem for elem in wav_info.mean_of_features]).strip("[]")
@@ -90,5 +96,8 @@ def write_output(wav_list):
 
             #writing the line on the file
             output_file.write(line + "\n")
+
+            i += 1
+
 
 
